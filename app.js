75f0ffa06812c53ef9ca73edec16ffe0c18852f0
ianagez <ianagez88@gmail.com>
2017-08-21 10:16:04 +0300

    XLSX FILE READY

--- app.js
+++ app.js
@@ -19,53 +19,50 @@ app.use('/',express.static('public'));
 var ConfigurationError, DBConnectError;
 
 
+//app.get('/', function (req, res) {
+//
+//    res.sendFile(path.join(__dirname, '/load_file.html'));
+//
+//});
+
 app.get('/', function (req, res) {
 
-  //  var fname = tempfile('.xlsx');
+    var data =JSON.parse(fs.readFileSync("/home/ianagez/IdeaProjects/chat/testInfoTable.json"));
 
+    var fname = path.join(__dirname, '/out.xlsx');
 
-    var wb=getWorkBook(JSON.parse(fs.readFileSync("/home/ianagez/IdeaProjects/chat/testInfoTable.json")));
-    function getWorkBook(data){
-        //var fname = tempfile('.xlsx');
-       var fname= path.join(__dirname,'/out.xlsx');
-
-        fs.openSync(fname, 'w');
-
-       // fs.writeFileSync(fname,"");
-       // var wb = XLSX.readFile('/home/ianagez/IdeaProjects/chat/out.xlsx');
-       // var fname= tempy.file({extension: 'xlsx'});
-
+    fs.open(fname, 'w', function (err) {
+        if (err){
+            console.log("err=", err);
+            res.end(); /////
+            return;
+        }
         var wb = XLSX.readFile(fname);
+        wb.SheetNames=[];
         var ws;
-        for (var i in data){
-                  var jsonObj=data[i];
-            ws = XLSX.utils.json_to_sheet(jsonObj.data, jsonObj.headers );
+        for (var i in data) {
+            var jsonObj = data[i];
+            ws = XLSX.utils.json_to_sheet(jsonObj.data, jsonObj.headers);
             wb.SheetNames.push(jsonObj.id);
             wb.Sheets[jsonObj.id] = ws;
         }
-       // return
         XLSX.writeFile(wb, fname);
-       // res.end();
-
-        res.sendFile(fname);
-        fs.unlinkSync(fname);
-    }
-
+        var options = {
+            headers: {
+                'Content-Disposition': 'attachment; filename =out.xlsx'
+            }
+        };
+        res.sendFile(fname, options, function(err){
+            if(err){
+                console.log("err=",err);
+                res.end(); /////
+                return;
+            }
+            fs.unlinkSync(fname); console.log("fname unlinkSync=", fname);
+        });
+    });
 });
 
-//function process_RS(stream/*:ReadStream*/,cb/*:(wb:Workbook)=>void*/)/*:void*/{
-//    var buffers = [];
-//    stream.on('data', function(data) { buffers.push(data); });
-//    stream.on('end', function() {
-//        var buffer = Buffer.concat(buffers);
-//        var workbook = XLSX.read(buffer, {type:"buffer"});
-//
-//        /* DO SOMETHING WITH workbook IN THE CALLBACK */
-//
-//
-//        cb(workbook);
-//    });
-//}
 
 app.listen(8086, function (err) {
 });
